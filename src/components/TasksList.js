b7f1060b1faef3365fc0413bab698a916684739e
Kiu L <zqdummy@gmail.com>
2020-11-15 22:37:32 -0800

    Add function to task edit button in TasksList

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -4,72 +4,15 @@ import {MdArrowDropDownCircle} from 'react-icons/md'
 import {navigate} from '@reach/router'
 
 // -- This compenent displays a list of tasks from a filtered Array passed on to it from Dashboard
-export default function TasksList(prop) {
-	// *** disable props for now
+export default function TasksList({filteredTasksArray}) {
 	// *** Feature - change background colour based on priority
 
-	// *** sample filtered tasks array from Dashboard
-	let tasksArray = [
-		{
-			id: '4wsqXU',
-			taskName: 'Suspendisse potenti.',
-			priority: 'High',
-			status: 'Archieved',
-			dateCreated: '2020-08-27',
-			dateModified: '2020-06-05',
-			dateDue: '2020-03-12',
-			description:
-				'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.',
-			supervisorId: 'user2',
-			responsibleId: 'user1',
-			commentListId: 'aIfCnh',
-		},
-		{
-			id: 'zQ0wwr',
-			taskName:
-				'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-			priority: 'High',
-			status: 'Archieved',
-			dateCreated: '2020-01-10',
-			dateModified: '2020-02-16',
-			dateDue: '2020-03-12',
-			description:
-				'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.',
-			supervisorId: 'user2',
-			responsibleId: 'user1',
-			commentListId: '1USQQR',
-		},
-		// {
-		// 	id: 'yRdat4',
-		// 	taskName: 'Nunc nisl.',
-		// 	priority: 'Low',
-		// 	status: 'Archieved',
-		// 	dateCreated: '2020-02-06',
-		// 	dateModified: '2020-01-27',
-		// 	dateDue: '2020-03-12',
-		// 	description:
-		// 		'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.',
-		// 	supervisorId: 'user2',
-		// 	responsibleId: 'user1',
-		// 	commentListId: '7ZnMis',
-		// },
-		// {
-		// 	id: 'haBkOA',
-		// 	taskName: 'Nullam porttitor lacus at turpis.',
-		// 	priority: 'Low',
-		// 	status: 'Archieved',
-		// 	dateCreated: '2019-12-26',
-		// 	dateModified: '2020-08-10',
-		// 	dateDue: '2020-03-12',
-		// 	description:
-		// 		'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.',
-		// 	supervisorId: 'user4',
-		// 	responsibleId: 'user1',
-		// 	commentListId: '8Aikbc',
-		// },
-	]
+	function handleClick(e, taskId) {
+		e.stopPropagation()
+		navigate('editTask/' + taskId)
+	}
 
-	let myTask = tasksArray.map((task) => {
+	let myTask = filteredTasksArray.map((task) => {
 		return (
 			<div className="card" key={task.id}>
 				<div className="card-header p-0" id={task.id}>
@@ -85,6 +28,9 @@ export default function TasksList(prop) {
 							type="button"
 							className="btn btn-lg btn-outline-secondary col-1 p-2"
 							title="Edit Task"
+							onClick={(e) => {
+								handleClick(e, task.id)
+							}}
 						>
 							<RiFileEditLine />
 						</button>
