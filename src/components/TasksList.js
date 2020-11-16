73b7f0a16a522102195ae5b25375e530f45f5ee5
Kiu L <zqdummy@gmail.com>
2020-11-15 21:42:28 -0800

    Add more info to the expanded TasksList items

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -6,16 +6,18 @@ import {navigate} from '@reach/router'
 // -- This compenent displays a list of tasks from a filtered Array passed on to it from Dashboard
 export default function TasksList(prop) {
 	// *** disable props for now
+	// *** Feature - change background colour based on priority
 
 	// *** sample filtered tasks array from Dashboard
 	let tasksArray = [
 		{
 			id: '4wsqXU',
 			taskName: 'Suspendisse potenti.',
-			priority: 'P2',
+			priority: 'High',
 			status: 'Archieved',
 			dateCreated: '2020-08-27',
 			dateModified: '2020-06-05',
+			dateDue: '2020-03-12',
 			description:
 				'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.',
 			supervisorId: 'user2',
@@ -26,42 +28,45 @@ export default function TasksList(prop) {
 			id: 'zQ0wwr',
 			taskName:
 				'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-			priority: 'P2',
+			priority: 'High',
 			status: 'Archieved',
 			dateCreated: '2020-01-10',
 			dateModified: '2020-02-16',
+			dateDue: '2020-03-12',
 			description:
 				'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.',
 			supervisorId: 'user2',
 			responsibleId: 'user1',
 			commentListId: '1USQQR',
 		},
-		{
-			id: 'yRdat4',
-			taskName: 'Nunc nisl.',
-			priority: 'P1',
-			status: 'Archieved',
-			dateCreated: '2020-02-06',
-			dateModified: '2020-01-27',
-			description:
-				'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.',
-			supervisorId: 'user2',
-			responsibleId: 'user1',
-			commentListId: '7ZnMis',
-		},
-		{
-			id: 'haBkOA',
-			taskName: 'Nullam porttitor lacus at turpis.',
-			priority: 'P1',
-			status: 'Archieved',
-			dateCreated: '2019-12-26',
-			dateModified: '2020-08-10',
-			description:
-				'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.',
-			supervisorId: 'user4',
-			responsibleId: 'user1',
-			commentListId: '8Aikbc',
-		},
+		// {
+		// 	id: 'yRdat4',
+		// 	taskName: 'Nunc nisl.',
+		// 	priority: 'Low',
+		// 	status: 'Archieved',
+		// 	dateCreated: '2020-02-06',
+		// 	dateModified: '2020-01-27',
+		// 	dateDue: '2020-03-12',
+		// 	description:
+		// 		'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.',
+		// 	supervisorId: 'user2',
+		// 	responsibleId: 'user1',
+		// 	commentListId: '7ZnMis',
+		// },
+		// {
+		// 	id: 'haBkOA',
+		// 	taskName: 'Nullam porttitor lacus at turpis.',
+		// 	priority: 'Low',
+		// 	status: 'Archieved',
+		// 	dateCreated: '2019-12-26',
+		// 	dateModified: '2020-08-10',
+		// 	dateDue: '2020-03-12',
+		// 	description:
+		// 		'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.',
+		// 	supervisorId: 'user4',
+		// 	responsibleId: 'user1',
+		// 	commentListId: '8Aikbc',
+		// },
 	]
 
 	let myTask = tasksArray.map((task) => {
@@ -99,7 +104,15 @@ export default function TasksList(prop) {
 						aria-labelledby={task.id}
 						data-parent="#oneTaskElement"
 					>
-						<div className="card-body">{task.description}</div>
+						<div className="card-body text-left ">
+							<div className="container">
+								<div className="card-title row justify-content-between">
+									<span>Priority: {task.priority}</span>
+									<span>Due Date: {task.dateDue}</span>
+								</div>
+							</div>
+							<p className="card-text">{task.description}</p>
+						</div>
 					</div>
 				</div>
 			</div>
