66349c1086b5a4bbcdb2a294c0c66c6dda974f54
Kiu <zqdummy@gmail.com>
2020-12-23 23:57:41 -0800

    Refactor useTaskList
    Move 2 useEffect out main body and combine them into a single custom hook

--- src/services/task/useTaskList.js
+++ src/services/task/useTaskList.js
@@ -3,21 +3,25 @@ import firebase from 'firebase/app'
 import {getCurrentAuthUser} from '../firebaseAuth/getCurrentAuthUser'
 /*
 A wrapper API that RETURNS a realtime array of task Objects associated with the currently logged on userId
-ex: [{dateCreated, dateDue, nameResponsible, nameResponsibleId, nameTaskCreator, nameTaskCreatorId, priority, status, taskDescription, taskName}]
 NOTE refer to the comments in useComments for detail innerworkings of the code below
 */
 export const useTaskList = () => {
-	let userAuthObj = getCurrentAuthUser()
-	let finalList = []
-	const [tasksCreator, setTasksCreator] = useState([])
-	const [tasksResponsible, setTasksResponsible] = useState([])
+	const tasksCreator = useUserBelongToTask('nameTaskCreatorId')
+	const tasksResponsible = useUserBelongToTask('nameResponsibleId')
+	let finalList = [...tasksCreator, ...tasksResponsible]
+	return finalList
+}
 
+// -- Param userTypeFilter is either 'nameTaskCreatorId' or 'nameResponsibleId'
+const useUserBelongToTask = (userTypeFilter) => {
+	let userAuthObj = getCurrentAuthUser()
+	const [tasks, setTasks] = useState([])
 	// -- loading Docs where current userId match the creator field
 	useEffect(() => {
 		let tasksArray = []
 		const tasksRef = firebase.firestore().collection('tasks')
 		const unsubscribe = tasksRef
-			.where('nameTaskCreatorId', '==', userAuthObj.id)
+			.where(userTypeFilter, '==', userAuthObj.id)
 			.onSnapshot((snapshot) => {
 				tasksArray = snapshot.docs.map((doc) => ({
 					id: doc.id,
@@ -29,57 +33,25 @@ export const useTaskList = () => {
 					let simpleDate = obj.dateCreated.toDate()
 					simpleDate = simpleDate.toISOString().slice(0, 10)
 					obj.dateCreated = simpleDate
-					return obj
+					// -- curate what will be returned so we can control future changes better
+					return {
+						id: obj.id,
+						dateCreated: obj.dateCreated,
+						dateDue: obj.dateDue,
+						priority: obj.priority,
+						status: obj.status,
+						nameResponsible: obj.nameResponsible,
+						nameResponsibleId: obj.nameResponsibleId,
+						nameTaskCreator: obj.nameTaskCreator,
+						nameTaskCreatorId: obj.nameTaskCreatorId,
+						taskDescription: obj.taskDescription,
+						taskName: obj.taskName,
+					}
 				})
-				setTasksCreator(tasksArray)
+				setTasks(tasksArray)
 			})
 		return () => unsubscribe()
 		// eslint-disable-next-line
 	}, [])
-	// -- loading Docs where current userId match the responsible field
-	useEffect(() => {
-		let tasksArray = []
-		const tasksRef = firebase.firestore().collection('tasks')
-		const unsubscribe = tasksRef
-			.where('nameResponsibleId', '==', userAuthObj.id)
-			.onSnapshot((snapshot) => {
-				tasksArray = snapshot.docs.map((doc) => ({
-					id: doc.id,
-					...doc.data({serverTimestamps: 'estimate'}),
-				}))
-				// -- convert the Firestore timestamp value to a simplified date STRING of YYYY-MM-DD
-				tasksArray = tasksArray.map((obj) => {
-					// -- creates a new Date() from the milliseconds Firestore Timestamp
-					let simpleDate = obj.dateCreated.toDate()
-					simpleDate = simpleDate.toISOString().slice(0, 10)
-					obj.dateCreated = simpleDate
-					return obj
-				})
-				setTasksResponsible(tasksArray)
-			})
-		return () => unsubscribe()
-		// eslint-disable-next-line
-	}, [])
-
-	finalList = [...tasksCreator, ...tasksResponsible]
-	return finalList
+	return tasks
 }
-
-/*******************************************************************/
-// *** bring useeffect outside as a custom hook inside a custom hook
-/*******************************************************************/
-
-// ;[
-// 	{
-// 		dateCreated,
-// 		dateDue,
-// 		nameResponsible,
-// 		nameResponsibleId,
-// 		nameTaskCreator,
-// 		nameTaskCreatorId,
-// 		priority,
-// 		status,
-// 		taskDescription,
-// 		taskName,
-// 	},
-// ]
