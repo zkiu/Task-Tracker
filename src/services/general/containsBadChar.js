50682c405539bfa3ce509a6e479016e0ccaff26a
Kiu <zqdummy@gmail.com>
2020-12-24 19:31:36 -0800

    Add to regex to include ! ? .

--- src/services/general/containsBadChar.js
+++ src/services/general/containsBadChar.js
@@ -1,7 +1,8 @@
 /*
-Returns true if a string contains anything other than 0-9, a-z (case insensitive), and whitespace
+Returns true if a string contains anything other than 0-9, a-z (case insensitive), and whitespace.
+This will also accept !(u0021), ?(003F), and .(002E)
 */
 export const containsBadChar = (str) => {
-	var regex = /[^a-z0-9\s]/gi
+	var regex = /[^a-z0-9\s\u0021\u003F\u002E]/gi
 	return regex.test(str)
 }
