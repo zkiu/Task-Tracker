6d854df31f48e3956d7ca1b4c97a73312feaed8a
Kiu <zqdummy@gmail.com>
2021-05-18 13:38:01 -0700

    update RegisterPage

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,8 +1,7 @@
-import React, {useEffect, useState} from 'react'
+import {useState} from 'react'
 import {useHistory} from 'react-router-dom'
 
 import registerUser from '../services/user/registerUser'
-import ErrorForm from '../components/ErrorForm'
 
 function Register() {
 	let history = useHistory()
@@ -10,20 +9,8 @@ function Register() {
 		name: '',
 		email: '',
 		jobLevel: '',
-		passOne: '',
-		passTwo: '',
+		password: '',
 	})
-
-	const [errorMessage, setErrorMessage] = useState(null)
-
-	//checking passwors match
-	useEffect(() => {
-		if (regData.passOne !== regData.passTwo) {
-			setErrorMessage('Passwords do not match.')
-		} else {
-			setErrorMessage(null)
-		}
-	}, [regData.passOne, regData.passTwo])
 
 	function handleChange(e) {
 		setRegData({
@@ -35,19 +22,11 @@ function Register() {
 	async function handleSubmit(e) {
 		e.preventDefault()
 
-		// -- prevent the form submission the 2 passwords doesn't match
-		if (regData.passOne !== regData.passTwo) {
-			setErrorMessage(
-				'Passwords do not match. Please fix before re-submitting.'
-			)
-			return
-		}
-
 		let registrationInfo = {
 			name: regData.name,
 			email: regData.email,
 			jobLevel: regData.jobLevel,
-			password: regData.passOne,
+			password: regData.password,
 		}
 
 		// -- validation of required field and email syntax is done via htlm code
@@ -55,7 +34,6 @@ function Register() {
 		// -- validation of minimum required passwordcomplexity is done by firebase.auth()
 		await registerUser(registrationInfo)
 
-		// -- once registered, the user will be logged in and directed towards the dashboard
 		history.push('/dashboard')
 	}
 
@@ -67,105 +45,82 @@ function Register() {
 						<div className="card bg-light">
 							<div className="card-body">
 								<h3 className="font-weight-light mb-3">Register</h3>
-								<div className="form-row">
-									{/* *** Feature: make Error Message into modal/toast */}
-									{errorMessage ? (
-										<ErrorForm errorMessage={errorMessage} />
-									) : null}
-
-									<section className="col-sm-12 form-group">
-										<label
-											className="form-control mb-2-label visually-hidden"
-											htmlFor="name"
-										>
-											Name
-										</label>
-										<input
-											required
-											autoFocus
-											autoComplete="true"
-											className="form-control mb-2 border border-secondary"
-											type="text"
-											id="name"
-											placeholder="Name"
-											name="name"
-											value={regData.name}
-											onChange={handleChange}
-										/>
-									</section>
+								<div className="">
+									<label
+										className="form-control visually-hidden"
+										htmlFor="name"
+									>
+										Name
+									</label>
+									<input
+										required
+										autoFocus
+										autoComplete="true"
+										className="form-control mb-2 border border-secondary"
+										type="text"
+										id="name"
+										placeholder="Name"
+										name="name"
+										value={regData.name}
+										onChange={handleChange}
+									/>
 								</div>
-								<div className="form-row">
-									<section className="col-sm-6 form-group">
-										<label
-											className="form-control mb-2-label visually-hidden"
-											htmlFor="email"
-										>
-											Email
-										</label>
-										<input
-											required
-											autoComplete="true"
-											className="form-control mb-2 border border-secondary"
-											type="email"
-											id="email"
-											placeholder="Email Address"
-											name="email"
-											value={regData.email}
-											onChange={handleChange}
-										/>
-									</section>
-									<section className="col-sm-6 form-group">
-										<label
-											className="form-control mb-2-label visually-hidden"
-											htmlFor="name"
-										>
-											Job Level
-										</label>
-										<select
-											className="form-select border border-secondary mb-2"
-											name="jobLevel"
-											id="jobLevel"
-											value={regData.jobLevel}
-											onChange={handleChange}
-											required
-										>
-											{/* Form is prevented from being submitted when the job level is not selected thanks to the 'required' property above and the value='' below  */}
-											<option defaultValue value="">
-												Job Level...
-											</option>
-											<option value="L1">L1: General Employee</option>
-											<option value="L2">L2: Supervisor</option>
-										</select>
-									</section>
+								<div className="">
+									<label
+										className="form-control visually-hidden"
+										htmlFor="email"
+									>
+										Email
+									</label>
+									<input
+										required
+										autoComplete="true"
+										className="form-control mb-2 border border-secondary"
+										type="email"
+										id="email"
+										placeholder="Email Address"
+										name="email"
+										value={regData.email}
+										onChange={handleChange}
+									/>
 								</div>
-								<div className="form-row">
-									<section className="col-sm-6 form-group">
-										<input
-											required
-											autoComplete="true"
-											className="form-control mb-2 border border-secondary"
-											type="password"
-											name="passOne"
-											placeholder="Password"
-											value={regData.passOne}
-											onChange={handleChange}
-										/>
-									</section>
-									<section className="col-sm-6 form-group">
-										<input
-											required
-											autoComplete="true"
-											className="form-control mb-2 border border-secondary"
-											type="password"
-											name="passTwo"
-											placeholder="Repeat Password"
-											value={regData.passTwo}
-											onChange={handleChange}
-										/>
-									</section>
+								<div className="">
+									<input
+										required
+										autoComplete="true"
+										className="form-control mb-2 border border-secondary"
+										type="password"
+										name="password"
+										placeholder="Password"
+										value={regData.password}
+										onChange={handleChange}
+									/>
 								</div>
-								<div className="form-group text-right mb-0">
-									<button className="btn btn-secondary" type="submit">
+								<div className="">
+									<label
+										className="form-control visually-hidden"
+										htmlFor="name"
+									>
+										Job Level
+									</label>
+									<select
+										className="form-select border border-secondary mb-2"
+										name="jobLevel"
+										id="jobLevel"
+										value={regData.jobLevel}
+										onChange={handleChange}
+										required
+									>
+										{/* Form is prevented from being submitted when the job level is not selected thanks to the 'required' property above and the value='' below  */}
+										<option defaultValue value="">
+											Job Level...
+										</option>
+										<option value="L1">L1: General Employee</option>
+										<option value="L2">L2: Supervisor</option>
+									</select>
+								</div>
+								<div className=" text-end mb-0">
+									<button className="btn btn-primary" type="submit">
 										Register
 									</button>
 								</div>
